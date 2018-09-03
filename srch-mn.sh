3b7bf5c6eca89872640d3e3bf1c8206fb6e8fa59
searchcoindev <42806434+searchcoindev@users.noreply.github.com>
2018-09-03 17:02:21 +0400

    Update srch-mn.sh

--- srch-mn.sh
+++ srch-mn.sh
@@ -76,13 +76,13 @@ read PRIVKEY
 
 CONF_DIR=~/.searchcoin/
 CONF_FILE=searchcoin.conf
-PORT=19696
+PORT=19000
 
 mkdir -p $CONF_DIR
 echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` > $CONF_DIR/$CONF_FILE
 echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
 echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
-echo "rpcport=19670" >> $CONF_DIR/$CONF_FILE
+echo "rpcport=19001" >> $CONF_DIR/$CONF_FILE
 echo "listen=1" >> $CONF_DIR/$CONF_FILE
 echo "server=1" >> $CONF_DIR/$CONF_FILE
 echo "daemon=1" >> $CONF_DIR/$CONF_FILE
